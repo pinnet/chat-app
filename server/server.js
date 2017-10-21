d49f83ee7d16f8d531ba0d6736c9f470fecab9db
Danny Arnold <danny@dannyarnold.com>
2017-10-21 14:52:31 +0100

    made connect disconnect handlers

--- server/server.js
+++ server/server.js
@@ -1,12 +1,23 @@
 const path = require('path');
+const http = require('http');
+const socketIO = require('socket.io');
 const pubPath = path.join(__dirname,'../public');
 const express = require('express');
 
 const port = process.env.PORT || 3000;
 
 var app = express();
-console.log(pubPath);
+var server = http.createServer(app);
+var io = socketIO(server);
+
 app.use(express.static(pubPath));
-app.listen(port, () =>{
-    console.log(`Starting server on port ${port}`);
+
+io.on('connection', (socket) => {
+  console.log('Socket:connect');
+  
+  socket.on('disconnect', () => {
+    console.log ('Disconnected from server');
+ });
 });
+ 
+server.listen(port, () =>{ console.log(`Starting server on port ${port}`);});
