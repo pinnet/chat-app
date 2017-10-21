f91fecd635ad8b647c878e451ec8545f1df3be4b
Danny Arnold <danny@dannyarnold.com>
2017-10-22 00:26:02 +0100

    Added moment to format time

--- server/utils/message.js
+++ server/utils/message.js
@@ -1,10 +1,12 @@
+var moment = require('moment');
+
 var generateMessage = (from,text) => {
   return {from,text,
-    createdAt: new Date().getTime()
+    createdAt: moment().valueOf()
   };  
 };
 
 var generateLocationMessage = (from,lat,lon) => {
-    return {from,url:`https://www.google.com/maps?q=${lat},${lon}`,createdAt: new Date().getTime()}
+    return {from,url:`https://www.google.com/maps?q=${lat},${lon}`,createdAt: moment().valueOf()}
 }
 module.exports = {generateMessage,generateLocationMessage};
